113120f655a5ebbe0dff4ff63bf9e8ef619d2362
Sina <sinasteyn@gmail.com>
2024-11-22 23:59:12 +0330

    add json-server add useFetch custom hook add LocationList component

---
+++ src/components/LocationList/LocationList.jsx
@@ -0,0 +1,35 @@
+import useFetch from "../../hooks/useFetch";
+import "./location-list.css";
+function LocationList() {
+  const { data, isLoading } = useFetch("http://localhost:3001/hotels");
+  if (isLoading) <p>Loading...</p>;
+
+  return (
+    <div className="nearby-locations">
+      <h2>Nearby Locations</h2>
+      <div className="location-list">
+        {data.map((item) => {
+          return (
+            <div className="location-item" key={item.id}>
+              <img
+                className="location-item__img"
+                src={item.picture_url.url}
+                alt={item.name}
+              />
+              <div className="location-item__desc">
+                <p className="location-item__location">{item.smart_location}</p>
+                <p className="location-item__name">{item.name}</p>
+                <p className="location-item__price">
+                  €&nbsp;{item.price}&nbsp;
+                  <span>night</span>
+                </p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default LocationList;
